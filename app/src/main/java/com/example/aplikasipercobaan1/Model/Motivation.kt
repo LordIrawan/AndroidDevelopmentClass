b6005f5d9eeef80c75a470d1ef890fd9cb007ff8
Irawan Kurnia <irawankur08@gmail.com>
2022-11-03 09:35:50 +0700

    03-11-2022

---
+++ app/src/main/java/com/example/aplikasipercobaan1/Model/Motivation.kt
@@ -0,0 +1,9 @@
+package com.example.aplikasipercobaan1.Model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+class Motivation(
+    @DrawableRes val imageResourceId: Int,
+    @StringRes val textResourceId: Int
+)
